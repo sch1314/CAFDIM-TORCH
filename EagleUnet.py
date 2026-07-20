8fc75d6ed1b950a932e0e2c9f50f150ff00c7f6a
sch1314 <70927676+sch1314@users.noreply.github.com>
2026-07-21 01:14:31 +0800

    Create EagleUnet.py

---
+++ EagleUnet.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+
+class EagleUNet(nn.Module):
+
+
+    def __init__(self):
+        super(EagleUNet, self).__init__()
+
+        def CBR(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+                nn.InstanceNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding),
+                nn.InstanceNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            )
+
+        self.encoder1 = CBR(1, 32)
+        self.encoder2 = CBR(32, 64)
+        self.encoder3 = CBR(64, 128)
+        self.encoder4 = CBR(128, 256)
+
+        self.bottom = CBR(256, 512)
+
+        self.up4 = CBR(512 + 256, 256)
+        self.up3 = CBR(256 + 128, 128)
+        self.up2 = CBR(128 + 64, 64)
+        self.up1 = CBR(64 + 32, 32)
+
+        self.final = nn.Conv2d(32, 1, kernel_size=1)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+    def forward(self, x):
+        # Encoding path
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.maxpool(enc1))
+        enc3 = self.encoder3(self.maxpool(enc2))
+        enc4 = self.encoder4(self.maxpool(enc3))
+
+        # Bottleneck
+        bottom = self.bottom(self.maxpool(enc4))
+
+        # Decoding path
+        dec4 = self.up4(torch.cat([self.upsample(bottom), enc4], dim=1))
+        dec3 = self.up3(torch.cat([self.upsample(dec4), enc3], dim=1))
+        dec2 = self.up2(torch.cat([self.upsample(dec3), enc2], dim=1))
+        dec1 = self.up1(torch.cat([self.upsample(dec2), enc1], dim=1))
+
+        return self.final(dec1)
